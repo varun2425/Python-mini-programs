23a81847cbd03bde70f2d4fdebbfcd557cc0e221
varun2425 <varunmahi030@gmail.com>
2026-04-30 11:04:10 +0530

    .

---
+++ Python-Day5-practice/Mini_Challenge.py
@@ -0,0 +1,16 @@
+def add(a, b):
+    return a + b
+
+def sub(a, b):
+    return a - b
+
+def mul(a, b):
+    return a * b
+
+def div(a, b):
+    return a / b
+
+print(add(10, 5))
+print(sub(10, 5))
+print(mul(10, 5))
+print(div(10, 5))
