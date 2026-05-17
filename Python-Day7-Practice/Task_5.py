b32bf05fc9840f27040135752d9a744f58f48862
varun2425 <varunmahi030@gmail.com>
2026-05-17 11:26:23 +0530

    Armstrong Number

---
+++ Python-Day7-Practice/Task_5.py
@@ -0,0 +1,14 @@
+num = int(input("Enter a number: "))
+
+temp = num
+total = 0
+
+while temp > 0:
+    digit = temp % 10
+    total += digit ** 3
+    temp //=10
+
+if total == num:
+    print("Armstrong Number")
+else:
+    ("Not Armstrong Number")
