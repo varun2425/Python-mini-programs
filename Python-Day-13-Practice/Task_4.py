9260377f8b32ff8c23e43540f87ab751fa156af9
varun2425 <varunmahi030@gmail.com>
2026-06-13 11:00:29 +0530

    .

---
+++ Python-Day-13-Practice/Task_4.py
@@ -0,0 +1,8 @@
+# Intergers
+age = 25
+quantity = 3
+num_of_students = 30
+
+print(f"You are (age) year old")
+print(f"You are buying {quantity} iteams")
+print(f"studentsyour class has {num_of_students} ")
---
+++ Python-Day-13-Practice/task_3.py
@@ -0,0 +1,8 @@
+# Strings
+first_name = "Varun"
+food = "briyani"
+email = "Varun24@gmail.com"
+
+print(f"Hello {first_name}")
+print(f"you like {food}")
+print(f"Your email is: {email}")
