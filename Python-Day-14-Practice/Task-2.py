05be98ec21649ad061bb8600773d476999a4049d
varun2425 <varunmahi030@gmail.com>
2026-06-15 11:29:35 +0530

    .

---
+++ Python-Day-14-Practice/Task-2.py
@@ -0,0 +1,15 @@
+name = "Thalari Varun"
+age = 23
+gpa = 7.2
+is_student = True
+
+gpa = int(gpa)
+#age = float(age)
+age = str(age)
+name = bool(name)
+
+
+print(gpa)
+#print(age)
+print(type(age))
+print(name)
