1dc644d30abf43af36caefb455b369ff02936031
varun2425 <varunmahi030@gmail.com>
2026-06-15 11:22:27 +0530

    .

---
+++ Python-Day-14-Practice/Task-1.py
@@ -0,0 +1,12 @@
+# typecasting = the process of converting a variable from one data type to another 
+#               str(),int(),float(),bool()
+
+name = "Thalari varun"
+age = 25
+gpa = 7.2
+is_student = True
+
+print(type(name))
+print(type(age))
+print(type(gpa))
+print(type(is_student))
